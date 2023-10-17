b335b006bbd757da8dbf69a992a9f446c8c8a215
nathaly3245 <127852587+nathaly3245@users.noreply.github.com>
2023-10-17 09:52:45 -0300

    Add files via upload

---
+++ aluramidi-main/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,17 @@
+
+
+
+
+function tocaSonPom (){ 
+     document.querySelector(`#som_tecla_pom`).play();
+    
+}
+
+const listaDeTeclas= document.querySelectorAl('.tecla');
+
+let contador= 0;
+
+while (contador <9) 
+   listaDeteclas[0]
+   
+document.querySelector(`.tecla_pom`);
